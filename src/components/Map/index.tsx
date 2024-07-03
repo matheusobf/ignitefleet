7d4ead027c059ee4974052f6ab4503a4c1c8458d
Matheus Santos <mat982005@gmail.com>
2024-07-03 17:11:41 -0300

    remove android folder and updates

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,4 +1,8 @@
-import MapView, { PROVIDER_GOOGLE, MapViewProps, LatLng } from "react-native-maps"
+import MapView, { PROVIDER_GOOGLE, MapViewProps, LatLng, Marker, Polyline } from "react-native-maps"
+import { IconBox } from "../IconBox"
+import { Car, FlagCheckered } from "phosphor-react-native"
+import { useRef } from "react"
+import { useTheme } from "styled-components/native"
 
 type Props = MapViewProps & {
     coordinates: LatLng[]
@@ -6,12 +10,38 @@ type Props = MapViewProps & {
 
 export function Map({ coordinates, ...rest }: Props) {
     const lastCoordinate = coordinates[coordinates.length - 1]
+    const mapRef = useRef<MapView>(null)
+    const { COLORS } = useTheme()
+    async function onMapLoaded(){
+        if(coordinates.length > 1){
+            mapRef.current?.fitToSuppliedMarkers(["departure", "arrival"], {
+                edgePadding: {
+                    top: 50,
+                    right: 50,
+                    bottom: 50,
+                    left: 50
+                }
+            })
+        }
+    }
     return (
         <MapView
+            ref={mapRef}
             provider={PROVIDER_GOOGLE}
             style={{ width: "100%", height: 200 }}
-            region={{ latitude: lastCoordinate.latitude, longitude: lastCoordinate.longitude, latitudeDelta: 0.005, longitudeDelta: 0.005 }} 
-            {...rest}
-        />
+            region={{ latitude: lastCoordinate.latitude, longitude: lastCoordinate.longitude, latitudeDelta: 0.005, longitudeDelta: 0.005 }}
+            onMapLoaded={onMapLoaded}
+            {...rest}>
+            <Marker identifier="departure" coordinate={coordinates[0]}>
+                <IconBox icon={Car} size="SMALL" />
+            </Marker>
+            {coordinates.length > 1 && 
+            <>
+            <Marker identifier="arrival" coordinate={lastCoordinate}>
+                <IconBox icon={FlagCheckered} size="SMALL" />
+            </Marker>
+            <Polyline coordinates={[...coordinates]} strokeColor={COLORS.GRAY_700} strokeWidth={7} />
+            </>}
+        </MapView>
     )
 }
